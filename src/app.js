26aa1944a3097681321fad8c755bc9ca4f116aad
ashu <ashupatil1357@gmail.com>
2024-02-29 12:42:10 +0530

    code updated

--- src/app.js
+++ src/app.js
@@ -26,14 +26,30 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for(let i=0; i<players.length; i++){
+        detailedPlayers.push({
+            name: players[i],
+            strength: getRandomStrength(),
+            image: `images/super-${i+1}.png`,
+            type: getRandomType(~~(Math.random() * 2))
+        });
+    }
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
+    return Math.floor(Math.random() * 101);
     // Note: You can use Math.random() and Math.ceil()
+}
+
+// to get random from 0-1
+const getRandomType = (i) => {
+    if(i==0)
+        return "hero";
+    else
+        return "villain";
 }
 
 const buildPlayers = (players, type) => {
@@ -42,7 +58,17 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+    for(let i=0; i<players.length; i++){
+        if(players[i].type == type){
+            fragment += 
+                `<div class="player">
+                    <img src="${players[i].image}">
+                    <div class="name">${players[i].name}</div>
+                    <div class="strength">${players[i].strength}</div>
+                 </div>
+                `
+        }
+    }
     return fragment;
 }
 // Display players in HTML
